a4f2c2da2cebd9db5760590e5d3913fff95573c9
tengsong <shawnt22@gmail.com>
2013-02-18 17:02:47 +0800

    Signed-off-by: tengsong <shawnt22@gmail.com>

--- first.php
+++ first.php
@@ -2,18 +2,21 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC_CN">
-<title>Insert title here</title>
+<title>Using Echo()</title>
 </head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
-
-	?>
-    </body>
-    </noframes>
-</frameset>
+<body>
+</body>
+<?php
+// 	echo "This was generated using PHP!";
+// 	echo "<br />"; 
+// 	echo "<b>Hello <font size='+2'>World</font>\n";
+// 	echo "<br />";
+	
+// 	$newfile = $_SERVER["PHP_SELF"];
+// 	$user = $_SERVER["HTTP_USER_AGENT"];
+// 	$address = $_SERVER["REMOTE_ADDR"];
+// 	echo "$newfile<br />$user<br />$address<br />".strlen($newfile)."<br />";
+// 	echo "$user. $address";
+	
+?>
 </html>
