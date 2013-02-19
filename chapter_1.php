14abcc351fd6be10169032fe2891872d006a4859
tengsong <shawnt22@gmail.com>
2013-02-19 10:47:54 +0800

    第一章联系
    Change-Id: Iab7283f787b22da19bf32f9baf9c777fcace224a

---
+++ chapter_1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC_CN">
+<title>Using Echo()</title>
+</head>
+<body>
+</body>
+<?php
+//	输出
+// 	echo "This was generated using PHP!";
+// 	echo "<br />"; 
+// 	echo "<b>Hello <font size='+2'>World</font>\n";
+// 	echo "<br />";
+	
+//	变量
+// 	$newfile = $_SERVER["PHP_SELF"];
+// 	$user = $_SERVER["HTTP_USER_AGENT"];
+// 	$address = $_SERVER["REMOTE_ADDR"];
+// 	echo "$newfile<br />$user<br />$address<br />".strlen($newfile)."<br />";
+// 	echo "$user. $address";
+
+//	连接符
+// 	$first_name = 'Shawn';
+// 	$last_name = 'Teng';
+// 	echo 'My name is '.$first_name.' '.$last_name;
+	
+//	数字
+// 	$n = 3.14;
+// 	$n = round($n);
+// 	echo $n."<br />";
+// 	$n = 3.1415926;
+// 	$n = number_format($n, 2);
+// 	echo $n."<br />";
+// 	$n = 1234567.1415926;
+// 	$n = number_format($n, -2);
+// 	echo $n;
+
+//	常量
+// 	define("USERNAME", "shawnteng");
+// 	echo USERNAME;
+	
+//	引号 ""内的值会被解释，''则按字面值处理 尽量使用''效率高
+// 	$value = "test";
+// 	echo "$value"."<br />";
+// 	echo "\$value"."<br />";
+// 	echo '$value'."<br />";
+// 	echo '\$value';
+	
+//	heredoc    <<< 结束符与开始符必须相同 结束符必须顶格 结束符后加;
+// 	$value = "test";
+// 	$value2 = <<< EOT
+// 	"this is"$value
+// 	"balabalabala"
+// EOT;
+// 	echo $value2;
+
+	
+?>
+</html>
